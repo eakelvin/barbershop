a747802dd32bb035a7a2fa9a271173a35de16d67
Kelvin Akaba <akabakelvin@gmail.com>
2024-10-16 12:12:01 +0000

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 const NavigationBar = () => {
   return (
     <div>
-        <nav 
+        {/* <nav 
             className="navbar navbar-expand-lg bg-body-tertiary hero-header"
             data-bs-theme="dark" 
         >
@@ -52,31 +52,32 @@ const NavigationBar = () => {
                     </span>
                 </div>
             </div>
-        </nav>
+        </nav> */}
 
-        {/* <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar 
+            expand="lg" 
+            className="bg-body-tertiary hero-header" 
+            data-bs-theme="dark"
+        >
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">BARBERSHOP</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                        Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                        Separated link
-                    </NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
+                <Navbar.Collapse className='justify-content-center' id="basic-navbar-nav">
+                    <Nav className="mx-auto">
+                        <Nav.Link className='active' href="/">Home</Nav.Link>
+                        <Nav.Link href="/service">Services</Nav.Link>
+                        <Nav.Link href="/about">About Us</Nav.Link>
+                        <Nav.Link href="/blog">Blog</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <button type="button" className="btn btn-outline-warning text-bright rounded-0">
+                            BOOK APPOINTMENT
+                        </button>
+                    </span>
                 </Navbar.Collapse>
             </Container>
-        </Navbar> */}
+        </Navbar>
     </div>
   )
 }

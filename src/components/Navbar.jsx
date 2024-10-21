27c1b5304c2eefdb6c1e9653161c222309a747ea
Kelvin Akaba <akabakelvin@gmail.com>
2024-10-21 18:13:56 +0000

    nav fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,19 @@ import { Link, NavLink } from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Button, Stack } from 'react-bootstrap';
+import { Button, Dropdown, Spinner, Stack } from 'react-bootstrap';
 import axios from 'axios';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavigationBar = () => {
     const apiUrl = import.meta.env.VITE_PUBLIC_API_BASE_URL;
     const [user, setUser] = useState(null);
-    // const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
 
+    const getInitial = (name) => {
+        return name ? name.charAt(0) : ''
+    }
+    
     useEffect(() => {
         const getUser = async () => {
             try {
@@ -18,18 +23,16 @@ const NavigationBar = () => {
                     `${apiUrl}/auth/user`, 
                     { withCredentials: true }
                 )
-                console.log('User Data:', response)
+                console.log('User Data:', response.data)
                 setUser(response.data)
             } catch (error) {
                 console.error('Error fetching profile:', error);
+            } finally {
+                setLoading(false)
             }
         }
         getUser()
     }, [])
-
-    useEffect(() => {
-        console.log("Fulfilled Promise:", user);
-    }, [user])
 
   return (
     <div>
@@ -51,37 +54,66 @@ const NavigationBar = () => {
                     </Nav>
 
                     <Stack className="col-12 d-lg-none" gap={3}>
-                        {user ? (
-                            <p className='!text-white'>{user?.name}</p>
+                        {loading ? (
+                            <Spinner animation="border" variant='light' />
+                        ): user ? (
+                            <Dropdown align={'start'}>
+                                <Dropdown.Toggle id="dropdown-button-dark-example1" variant="success">
+                                    {getInitial(user?.name)}
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="/appointment" active>
+                                        Book Appointment
+                                    </Dropdown.Item>
+                                    <Dropdown.Item href="/">Dashboard</Dropdown.Item>
+                                    <Dropdown.Divider />
+                                    <Dropdown.Item href="">Logout</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
                         ): (
-                            <Link to={'/login'}>
-                                <Button variant='dark' className='w-100 rounded-0'>
-                                    Login
-                                </Button>
-                            </Link>
+                            <div>
+                                <Link to={'/login'}>
+                                    <Button variant='dark' className='w-100 rounded-0'>
+                                        Login
+                                    </Button>
+                                </Link>
+                                <Link to={'/appointment'}>
+                                    <button type="button" className="w-100 btn btn-outline-warning text-bright rounded-0">
+                                        BOOK APPOINTMENT
+                                    </button>
+                                </Link>
+                            </div>
                         )}
-                        <Link to={'/appointment'}>
-                            <button type="button" className="w-100 btn btn-outline-warning text-bright rounded-0">
-                                BOOK APPOINTMENT
-                            </button>
-                        </Link>
                     </Stack>
 
                     <Stack className='d-none d-lg-block' direction="horizontal">
-                        {user ? (
-                            <p className='!text-white'>HI</p>
+                        {loading ? (
+                            <Spinner animation="border" variant='light' />
+                        ): user ? (
+                            <NavDropdown 
+                                title={user?.name} 
+                                id="basic-nav-dropdown" 
+                                className='text-white my-2'
+                            >
+                                <NavDropdown.Item href="/appointment">Book Appointment</NavDropdown.Item>
+                                <NavDropdown.Item href="/">Dashboard</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="">Logout</NavDropdown.Item>
+                            </NavDropdown>
                         ): (
-                            <Link to={'/login'}>
-                                <Button variant='dark' className='rounded-0'>
-                                    Login
-                                </Button>
-                            </Link>
+                            <div>
+                                <Link to={'/login'}>
+                                    <Button variant='dark' className='rounded-0'>
+                                        Login
+                                    </Button>
+                                </Link>
+                                <Link to={'/appointment'}>
+                                    <button type="button" className="btn btn-outline-warning text-bright rounded-0">
+                                        BOOK APPOINTMENT
+                                    </button>
+                                </Link>
+                            </div>
                         )}
-                        <Link to={'/appointment'}>
-                            <button type="button" className="btn btn-outline-warning text-bright rounded-0">
-                                BOOK APPOINTMENT
-                            </button>
-                        </Link>
                     </Stack>
                 </Navbar.Collapse>
             </Container>
